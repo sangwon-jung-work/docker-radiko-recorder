e4927b444b3d4c4e8a9f8fbeeb8def00a203e3e3
JUNG <sangwon.jung.work@gmail.com>
2020-12-08 04:28:44 +0900

    v2 auth api URL change, radiko recording method change(swf to hls)

--- rec_radiko2.sh
+++ rec_radiko2.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
+# 2020.12.07 change auth urls, recording method(swf -> m3u8, wget -> curl)
 
 pid=$$
-date=`date '+%Y-%m-%d-%H_%M'`
-playerurl=http://radiko.jp/apps/js/flash/myplayer-release.swf
-playerfile="/tmp/pre_player.swf"
-keyfile="/tmp/pre_authkey.png"
-cookiefile="/tmp/pre_cookie_${pid}_${date}.txt"
-loginfile="/tmp/pre_login.txt"
-checkfile="/tmp/pre_check.txt"
-logoutfile="/tmp/pre_logout.txt"
+filedate=`date '+%Y%m%d'`
+tempfiledate=`date '+%Y%m%d_%H%M%S'`
+
 
 outdir="."
+
 
 #
 # Logout Function
 #
 Logout () {
-   wget -q \
-     --header="pragma: no-cache" \
-     --header="Cache-Control: no-cache" \
-     --header="Expires: Thu, 01 Jan 1970 00:00:00 GMT" \
-     --header="Accept-Language: ja-jp" \
-     --header="Accept-Encoding: gzip, deflate" \
-     --header="Accept: application/json, text/javascript, */*; q=0.01" \
-     --header="X-Requested-With: XMLHttpRequest" \
-     --no-check-certificate \
-     --load-cookies $cookiefile \
-     --save-headers \
-     -O $logoutfile \
-     https://radiko.jp/ap/member/webapi/member/logout
+  curl -H "Accept-Language: ja-jp" \
+       -H "Accept-Encoding: gzip, deflate" \
+       -H "X-Requested-With: XMLHttpRequest" \
+       -b $cookiefile \
+       https://radiko.jp/ap/member/webapi/member/logout
 
     if [ -f $cookiefile ]; then
         rm -f $cookiefile
@@ -57,14 +46,24 @@ if [ $# -ge 6 ]; then
   PREFIX=$6
 fi
 
+
+#loginfile="${outdir}/${PREFIX}_${tempfiledate}_login.txt"
+cookiefile="${outdir}/${PREFIX}_${tempfiledate}_cookie.txt"
+keyfile="${outdir}/${PREFIX}_${tempfiledate}_authkey.txt"
+checkfile="${outdir}/${PREFIX}_${tempfiledate}_check.txt"
+auth1header="${outdir}/${PREFIX}_${tempfiledate}_auth1Header.txt"
+auth2file="${outdir}/${PREFIX}_${tempfiledate}_auth2.txt"
+logoutfile="${outdir}/${PREFIX}_${tempfiledate}_logout.txt"
+
+
+
+
 ###
-# radiko premium
+# radiko premium, generate cookie
 ###
 if [ $mail ]; then
-  wget -q --save-cookie=$cookiefile \
-       --keep-session-cookies \
-       --post-data="mail=$mail&pass=$pass" \
-       -O $loginfile \
+  curl -X POST -d "mail=$mail&pass=$pass" \
+       -c $cookiefile \
        https://radiko.jp/ap/member/login/login
 
   if [ ! -f $cookiefile ]; then
@@ -73,95 +72,37 @@ if [ $mail ]; then
   fi
 fi
 
-#
-# check login
-#
-wget -q \
-    --header="pragma: no-cache" \
-    --header="Cache-Control: no-cache" \
-    --header="Expires: Thu, 01 Jan 1970 00:00:00 GMT" \
-    --header="Accept-Language: ja-jp" \
-    --header="Accept-Encoding: gzip, deflate" \
-    --header="Accept: application/json, text/javascript, */*; q=0.01" \
-    --header="X-Requested-With: XMLHttpRequest" \
-    --no-check-certificate \
-    --load-cookies $cookiefile \
-    --save-headers \
-    -O $checkfile \
-    https://radiko.jp/ap/member/webapi/member/login/check
+###
+# check login status
+###
+curl -H "Accept-Encoding: gzip, deflate" \
+     -H "Accept-Language: ja-JP" \
+     -H "Cache-Control: no-cache" \
+     -b $cookiefile \
+     -o $checkfile \
+     https://radiko.jp/ap/member/webapi/member/login/check
 
 if [ $? -ne 0 ]; then
   echo "failed login"
   exit 1
 fi
 
-#
-# paid member
-#
-# jq
-# http://stedolan.github.io/jq/
-#
 
-#paid_member=`cat ${checkfile} | tail -2 | jq '.paid_member'`
-#areafree=`cat ${checkfile} | tail -2 | jq '.areafree'`
+# make keyfile
+echo 'bcd151073c03b352e1ef2fd66c32209da9ca0afa' > $keyfile
 
-#if [ $paid_member != "\"1\"" ]; then
-#   echo "inactive paid member"
-#   Logout
-#   exit 1
-#fi
 
-#if [ $areafree != "\"1\"" ]; then
-#   echo "area is not free"
-#   Logout
-#   exit 1
-#fi
-
-#
-# get player
-#
-if [ ! -f $playerfile ]; then
-  wget -q -O $playerfile $playerurl
-
-  if [ $? -ne 0 ]; then
-    echo "failed get player"
-    Logout
-    exit 1
-  fi
-fi
-
-#
-# get keydata (need swftool)
-#
-if [ ! -f $keyfile ]; then
-  swfextract -b 12 $playerfile -o $keyfile
-
-  if [ ! -f $keyfile ]; then
-    echo "failed get keydata"
-    Logout
-    exit 1
-  fi
-fi
-
-if [ -f auth1_fms_${pid} ]; then
-  rm -f auth1_fms_${pid}
-fi
-
-#
-# access auth1_fms
-#
-wget -q \
-     --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_ts" \
-     --header="X-Radiko-App-Version: 4.0.0" \
-     --header="X-Radiko-User: test-stream" \
-     --header="X-Radiko-Device: pc" \
-     --post-data='\r\n' \
-     --no-check-certificate \
-     --load-cookies $cookiefile \
-     --save-headers \
-     -O auth1_fms_${pid} \
-     https://radiko.jp/v2/api/auth1_fms
+###
+# access auth1
+###
+curl -H "pragma: no-cache" \
+     -H "X-Radiko-App: pc_html5" \
+     -H "X-Radiko-App-Version: 0.0.1" \
+     -H "X-Radiko-User: dummy_user" \
+     -H "X-Radiko-Device: pc" \
+     -b $cookiefile \
+     -D $auth1header \
+     https://radiko.jp/v2/api/auth1
 
 if [ $? -ne 0 ]; then
   echo "failed auth1 process"
@@ -169,41 +110,31 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-#
+###
 # get partial key
-#
-authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms_${pid}`
-offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1_fms_${pid}`
-length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1_fms_${pid}`
+###
+authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' ${auth1header}`
+offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' ${auth1header}`
+length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' ${auth1header}`
 
 partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
 
-echo "authtoken: ${authtoken} \noffset: ${offset} length: ${length} \npartialkey: $partialkey"
+echo -e "authtoken: ${authtoken} \n offset: ${offset} length: ${length} \n partialkey: $partialkey"
 
-rm -f auth1_fms_${pid}
 
-if [ -f auth2_fms_${pid} ]; then
-  rm -f auth2_fms_${pid}
-fi
 
 #
-# access auth2_fms
+# access auth2
 #
-wget -q \
-     --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_1" \
-     --header="X-Radiko-App-Version: 2.0.1" \
-     --header="X-Radiko-User: test-stream" \
-     --header="X-Radiko-Device: pc" \
-     --header="X-Radiko-Authtoken: ${authtoken}" \
-     --header="X-Radiko-Partialkey: ${partialkey}" \
-     --post-data='\r\n' \
-     --load-cookies $cookiefile \
-     --no-check-certificate \
-     -O auth2_fms_${pid} \
-     https://radiko.jp/v2/api/auth2_fms
+curl -H "X-Radiko-User: dummy_user" \
+     -H "X-Radiko-Device: pc" \
+     -H "X-Radiko-Authtoken: ${authtoken}" \
+     -H "X-Radiko-Partialkey: ${partialkey}" \
+     -b $cookiefile \
+     -o $auth2file \
+     https://radiko.jp/v2/api/auth2
 
-if [ $? -ne 0 -o ! -f auth2_fms_${pid} ]; then
+if [ $? -ne 0 -o ! -f $auth2file ]; then
   echo "failed auth2 process"
   Logout
   exit 1
@@ -211,47 +142,44 @@ fi
 
 echo "authentication success"
 
-areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms_${pid}`
+
+areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' ${auth2file}`
 echo "areaid: $areaid"
 
-rm -f auth2_fms_${pid}
 
-#
-# get stream-url
-#
+###
+# get hls stream url
+###
+stationinfo="${outdir}/${PREFIX}_${tempfiledate}_${channel}.xml"
 
-if [ -f ${channel}.xml ]; then
-  rm -f ${channel}.xml
+if [ -f $stationinfo ]; then
+  rm -f $stationinfo
 fi
 
-wget -q "http://radiko.jp/v2/station/stream/${channel}.xml"
+wget -q -O $stationinfo "https://radiko.jp/v3/station/stream/pc_html5/${channel}.xml"
 
-stream_url=`echo "cat /url/item[1]/text()" | xmllint --shell ${channel}.xml | tail -2 | head -1`
-url_parts=(`echo ${stream_url} | perl -pe 's!^(.*)://(.*?)/(.*)/(.*?)$/!$1://$2 $3 $4!'`)
+# first areafree url
+stream_url=`xmllint --xpath '//url[@timefree='0'][@areafree='1'][1]/playlist_create_url/text()' ${stationinfo}`
 
-rm -f ${channel}.xml
+# generate random id
+lsid=`date +%s999 -d '999999 seconds' | tr -d '\n' | md5sum | cut -d ' ' -f 1`
 
-#
-# rtmpdump
-#
-rtmpdump -v \
-         -r ${url_parts[0]} \
-         --app ${url_parts[1]} \
-         --playpath ${url_parts[2]} \
-         -W $playerurl \
-         -C S:"" -C S:"" -C S:"" -C S:$authtoken \
-         --live \
-         --stop ${DURATION} \
-         --flv "/tmp/${channel}_${date}"
+# add m3u8 parameters
+stream_param="?station_id=${channel}&l=15&lsid=${lsid}&type=b"
+
+
+# start recording & convert aac
+ffmpeg -headers "X-Radiko-AreaId: ${areaid}" -headers "X-Radiko-AuthToken: ${authtoken}" -loglevel info -y \
+       -i "${stream_url}${stream_param}" -acodec aac -ab 100k -t $DURATION "${outdir}/${PREFIX}_${filedate}.m4a"
+
+
+# delete temp files
+if [ $? = 0 ]; then
+  rm -f $keyfile $checkfile $auth1header $auth2file $stationinfo
+fi
 
 #
 # Logout
 #
-
 Logout
 
-ffmpeg -loglevel info -y -i "/tmp/${channel}_${date}" -acodec aac -ab 128k "${outdir}/${PREFIX}_${date}.m4a"
-
-if [ $? = 0 ]; then
-    rm -f "/tmp/${channel}_${date}"
-fi
